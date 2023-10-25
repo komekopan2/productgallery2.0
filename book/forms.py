12fd570f2f17728b153ffea0a0819df6483aa8b6
komekopan2 <yoo.shimizu888@gmail.com>
2023-10-25 22:37:20 +0900

    不要ファイルの削除

--- book/forms.py
+++
@@ -1,8 +0,0 @@
-""" from django import forms
-
-
-class CreateBookForm(forms.ModelForm):
-    class Meta:
-        model = Recipe
-        fields = ('title', 'text', 'thumbnail', 'category', 'user')
- """
