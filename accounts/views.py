7092f35f23b946070acb99d95cbe677d2142bd1f
komekopan2 <yoo.shimizu888@gmail.com>
2023-11-05 03:36:47 +0900

    サインアップ後ログイン状態になるよう改良

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
 from .forms import SignupForm
-from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.contrib.auth import login
 
-# Create your views here.
 
+# The function-based view for user signup
+def signup_view(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log the user in
+            login(request, user)
+            messages.success(request, "アカウントは正常に作成されました！")
+            # Redirect to the 'index' page after successful signup
+            return redirect(reverse("index"))
+    else:
+        form = SignupForm()
 
-class SignupView(SuccessMessageMixin, CreateView):
-    model = User
-    form_class = SignupForm  # form_classが定義されていないと、model=の中で定義されたモデルに基づいてModelFormが自動作成される
-    template_name = "accounts/signup.html"
-    success_url = reverse_lazy("index")
-    success_message = "サインアップが完了しました。ログインしましょう！"
+    return render(request, "accounts/signup.html", {"form": form})
