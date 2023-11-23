b7f49c5aab2ee22189ba9945c0cebc1aa3f4af14
komekopan2 <yoo.shimizu888@gmail.com>
2023-11-23 17:04:55 +0900

    静的ファイルをvolumesで共有管理

--- containers/django/entrypoint.sh
+++ containers/django/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 python manage.py makemigrations --noinput --settings bookproject.docker_settings
 python manage.py migrate --noinput --settings bookproject.docker_settings
-python manage.py collectstatic --noinput --settings bookproject.docker_settings
+# python manage.py collectstatic --noinput --settings bookproject.docker_settings
+python manage.py test --noinput --settings bookproject.docker_settings
 # 環境変数のDEBUGの値がTrueの時はrunserverを、Falseの時はgunicornを実行します
 # シェルスクリプトでは`[`と`$DEBUG`、`1`と`]`の間にスペースを一つ空けておかないと[]内の式を認識できないので注意
 echo $DEBUG
